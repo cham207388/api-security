b2bb64beea9c5c1079268fae79fde846fae2f82f
cham207388 <cham207388@gmail.com>
2021-09-22 14:49:29 -0400

    a student can only view their info and not another student's info

--- src/main/java/com/abc/apidemo/rest/controller/StudentController.java
+++ src/main/java/com/abc/apidemo/rest/controller/StudentController.java
@@ -1,10 +1,11 @@
 package com.abc.apidemo.rest.controller;
 
-import com.abc.apidemo.entity.StudentAppUser;
 import com.abc.apidemo.rest.StudentAppUserResponse;
 import com.abc.apidemo.service.StudentAppUserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1/students")
+@RequiredArgsConstructor
 public class StudentController {
 
-	@Autowired
-	private StudentAppUserService studentAppUserService;
+	private final StudentAppUserService studentAppUserService;
 
-	@GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAnyRole('ROLE_STUDENT')")
+	@GetMapping(path = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public StudentAppUserResponse findById(@PathVariable String id) {
 		return studentAppUserService.findById(id);
 	}
 
-	@GetMapping(path = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public StudentAppUser findByUsername(@PathVariable String username) {
-		return (StudentAppUser)studentAppUserService.loadUserByUsername(username);
+	@PreAuthorize("#username == authentication.principal")
+	@GetMapping(path = "/username", produces = MediaType.APPLICATION_JSON_VALUE)
+	public StudentAppUserResponse findByUsername(@Param("username") String username) {
+		return studentAppUserService.findByUsername(username);
 	}
 }
