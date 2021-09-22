1b77b5db48598d7bf98f7bd5c70e784060b4422a
cham207388 <cham207388@gmail.com>
2021-09-22 14:59:26 -0400

    find all app users by role

--- src/main/java/com/abc/apidemo/repo/StudentAppUserRepository.java
+++ src/main/java/com/abc/apidemo/repo/StudentAppUserRepository.java
@@ -1,11 +1,15 @@
 package com.abc.apidemo.repo;
 
 import com.abc.apidemo.entity.StudentAppUser;
+import com.abc.apidemo.security.AppUserRole;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentAppUserRepository extends MongoRepository<StudentAppUser, String> {
 
 	Optional<StudentAppUser> findByUsername(String username);
+
+	List<StudentAppUser> findAllByRole(AppUserRole role);
 }
